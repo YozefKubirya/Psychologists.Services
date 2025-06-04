6d3214635abbcd111287dcc52339ca70e30a1703
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2025-06-04 23:40:56 +0300

    render some markup for psychologists
    render some markup for psychologists

---
+++ src/components/PsychologistItem/PsychologistItem.jsx
@@ -0,0 +1,21 @@
+export default function PsychologistItem ({name,price,rating,experience,license,specialization,initial_consultation,about,reviews}) {
+   return (<>
+   <p>name:{name}</p>
+   <p>rating:{rating}</p>
+   <p>price:{price}</p>
+   <p>experience:{experience}</p>
+   <p>license:{license}</p>
+   <p>specialization:{specialization}</p>
+   <p>initial_consultation:{initial_consultation}</p>
+   <p>about:{about}</p>
+
+   <ul>reviews:{reviews.map((review,index)=>{
+      return <li key={index}>
+            <p>reviewer:{review.reviewer}</p>
+            <p>rating:{review.rating}</p>
+            <p>comment:{review.comment}</p>
+         </li>
+      
+   })}</ul>
+   </>)
+}
--- src/components/PsychologistsList/PsychologistsList.jsx
+++ src/components/PsychologistsList/PsychologistsList.jsx
@@ -1,8 +1,9 @@
+import PsychologistItem from "../PsychologistItem/PsychologistItem"
 export default function PsychologistsList({ psycologists }) {
    return (<>
    <ul>
       {psycologists.map((psy, index)=>{
-         return <li key={index}></li>
+         return <li key={index}><PsychologistItem name={psy.name} price={psy.price_per_hour} rating={psy.rating} experience={psy.experience} license={psy.license} specialization={psy.specialization} initial_consultation={psy.initial_consultation} about={psy.about} reviews={psy.reviews}/></li>
       })}
    </ul>
    </>)
